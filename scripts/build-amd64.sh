8fb9eeb0bcaea5bad6b0c364e89f293900bb2a7a
MrurBo <mrurbo2011@gmail.com>
2026-07-19 19:01:50 +0100

    Fixed amd64 script

--- scripts/build-amd64.sh
+++ scripts/build-amd64.sh
@@ -4,5 +4,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
 
-cmake -DCMAKE_BUILD_TYPE=Release --preset i386-release -S "$PROJECT_ROOT"
+cmake -DCMAKE_BUILD_TYPE=Release --preset release-i386 -S "$PROJECT_ROOT"
 cmake --build --preset i386-release
